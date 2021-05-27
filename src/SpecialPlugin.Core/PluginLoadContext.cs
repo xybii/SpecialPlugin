8610437607b72f0d319c3329619fda96a6243ed9
xybii <39405909+xybii@users.noreply.github.com>
2021-05-27 16:42:37 +0800

    Update

--- src/SpecialPlugin.Core/PluginLoadContext.cs
+++ src/SpecialPlugin.Core/PluginLoadContext.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 using System.Runtime.Loader;
 
@@ -11,18 +13,57 @@ namespace SpecialPlugin.Core
         public PluginLoadContext(string pluginPath)
         {
             _resolver = new AssemblyDependencyResolver(pluginPath);
+
+            Resolving += PluginLoadContext_Resolving;
+
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
         }
 
-        protected override Assembly Load(AssemblyName assemblyName)
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
-            string assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+            var assemblyName = new AssemblyName(args.Name);
+
+            string path = Path.Combine(Path.GetDirectoryName(args.RequestingAssembly?.Location), $"{assemblyName.Name}.dll");
+
+            if (File.Exists(path))
+            {
+                return Assembly.LoadFrom(path);
+            }
+
+            path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{assemblyName.Name}.dll");
+
+            if (File.Exists(path))
+            {
+                return Assembly.LoadFrom(path);
+            }
+
+            path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Path.GetFileName(args.RequestingAssembly?.Location));
+
+            if (File.Exists(path))
+            {
+                return Assembly.LoadFrom(path);
+            }
+
+            path = Path.Combine(Path.GetDirectoryName(args.RequestingAssembly?.Location), Path.GetFileName(args.RequestingAssembly?.Location));
+
+            if (File.Exists(path))
+            {
+                return Assembly.LoadFrom(path);
+            }
+
+            throw new DllNotFoundException();
+        }
+
+        private Assembly PluginLoadContext_Resolving(AssemblyLoadContext arg1, AssemblyName arg2)
+        {
+            string assemblyPath = _resolver.ResolveAssemblyToPath(arg2);
 
             if (assemblyPath != null)
             {
                 return LoadFromAssemblyPath(assemblyPath);
             }
 
-            return null;
+            return Default.LoadFromAssemblyName(arg2);
         }
 
         protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
